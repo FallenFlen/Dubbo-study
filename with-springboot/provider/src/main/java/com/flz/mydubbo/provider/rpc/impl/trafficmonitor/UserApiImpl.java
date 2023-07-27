a79997d305ab512088b35f08cda4c1f46909f1b0
Fallenz <beijuspsn@163.com>
2023-07-27 16:59:46 +0800

    chore: add more config

--- with-springboot/provider/src/main/java/com/flz/mydubbo/provider/rpc/impl/trafficmonitor/UserApiImpl.java
+++ with-springboot/provider/src/main/java/com/flz/mydubbo/provider/rpc/impl/trafficmonitor/UserApiImpl.java
@@ -6,13 +6,14 @@ import com.flz.mydubbo.dto.UserResponseDTO;
 import org.apache.dubbo.config.annotation.DubboService;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 // 默认1s超时
 @DubboService(group = "UserApi", version = "v1")
 public class UserApiImpl implements UserApi {
     @Override
     public UserResponseDTO login(String username, String password) throws InterruptedException {
-//        TimeUnit.MILLISECONDS.sleep(1500);
+        TimeUnit.MILLISECONDS.sleep(1500);
         return UserResponseDTO.builder()
                 .username(username)
                 .id(RandomUtils.uuid32())
